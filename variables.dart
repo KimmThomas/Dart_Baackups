69ce457c99e59648e880cf2b072cf00723e7fbff
KimmThomas <godie9050@gmail.com>
2024-02-24 21:45:31 +0300

    Backup
    1st Commit

---
+++ variables.dart
@@ -0,0 +1,20 @@
+void main() {
+  //declare variables
+  String name = 'dart class';
+  int age = 76;
+  double price = 10.62;
+  var year = 2024;
+  num students = 460;
+
+  //Do not quote in print-comment
+
+  /*
+  Comments also come here
+   */
+  print(name);
+  print(age);
+  print(price);
+  print(year);
+  print(students);
+
+}
